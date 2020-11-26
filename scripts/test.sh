86ab677201588188294cac36201f8aadd43b1e3b
James Curtin <jamescurtin@users.noreply.github.com>
2020-11-25 19:02:26 -0500

    Add codecov step to Github actions CI (#5)

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,4 +4,4 @@ set -euo pipefail
 CURRENT_DIR=$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)
 BASE_DIR="$(dirname "$CURRENT_DIR")"
 
-pytest --cov-report term-missing:skip-covered --cov-fail-under=95 --cov="${BASE_DIR}/app" "${BASE_DIR}/test" "$*"
+pytest --cov-report term-missing:skip-covered --cov-report=xml --cov-fail-under=95 --cov="${BASE_DIR}/app" "${BASE_DIR}/test" "$*"
